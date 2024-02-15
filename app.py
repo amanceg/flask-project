8380235f30c5fb299ab3e2398e4c10704bc8e9e3
AKAMN <115689684+AKAMN@users.noreply.github.com>
2024-02-15 05:53:00 +0000

    Added Dynamic Data

--- app.py
+++ app.py
@@ -1,11 +1,40 @@
-from flask import Flask
+from flask import Flask, request, render_template, jasonify
 
 app=Flask(__name__)
+JOBS=[
+    {   
+        'id':1,
+        'title':'Data Analyst',
+        'location':'Delhi',
+        'salary':'Rs. 100000'
+    },
+    {   
+        'id':1,
+        'title':'Data Analyst',
+        'location':'Delhi',
+        'salary':'Rs. 100000'
+
+    },
+    {   
+        'id':1,
+        'title':'Data Analyst',
+        'location':'Delhi',
+        'salary':'Rs. 100000'
+
+    }
+
+]
 
 @app.route("/")
+def hello_world():
+    return render_template('Home.html',
+                           jobs=JOBS)
+@app.route("/api/jobs")
+def job_list():
+    return jasonify(JOBS)
 
-def hello():
-  return "Hello World"
 
-if __name__=="__main__":
-  app.run(host="0.0.0.0",debug=True)
+
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', debug=True)
